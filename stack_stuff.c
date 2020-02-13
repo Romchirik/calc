942e17aada3e2dc0dfaef9a20dee80aa815a543b
Romchirick2 <r.titov@g.nsu.ru>
2020-02-13 09:54:31 +0700

    попытка перевода калькулятора на вещественные числа (новый стек)

--- stack_stuff.c
+++ stack_stuff.c
@@ -56,4 +56,61 @@ int is_empty(Stack_s *S) { //return 1 if stack is empty, and 0 if there is some 
     return (S->top == NULL);
 }
 
+struct list_f {
+    double data;
+    struct list_f *next;
+};
+
+typedef struct stack_f {
+    struct list_f *top;
+} Stack_sf;
+
+void clear_stack_f(Stack_sf *S) {
+    struct list_f *p;
+    while (S->top) {
+        p = S->top;
+        S->top = p->next;
+        free(p);
+    }
+}
+
+Stack_sf *stack_initialize_f() { // returns an empty stack
+    Stack_sf *S;
+    S = (Stack_sf *) malloc(sizeof(Stack_sf));
+    S->top = NULL;
+    return S;
+}
+
+double get_top_f(Stack_sf *S) { //return top value of stack
+    if (S->top)
+        return (S->top->data);
+    else
+        return 0;
+}
+
+double pop_f(Stack_sf *S) { //delete top stack value and returns it
+    double data;
+    struct list_f *tmp_top;
+
+    tmp_top = S->top;
+    data = tmp_top->data;
+    S->top = tmp_top->next;
+    free(tmp_top);
+
+    return data;
+}
+
+void push_f(double value, Stack_sf *S) { //puts value of value (lol) in top of stack S
+    struct list_f *p;
+    p = (struct list_f *) malloc(sizeof(struct list_f));
+    p->data = value;
+    p->next = S->top;
+    S->top = p;
+}
+
+int is_empty_f(Stack_sf *S) { //return 1 if stack is empty, and 0 if there is some date in stack
+    return (S->top == NULL);
+}
+
+
 #include "stack_stuff.h"
